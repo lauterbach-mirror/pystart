2b0f5941e74be14b84bcd5f594eadb0a1454e37d
THO <thomas.holzner@lauterbach.com>
2024-07-04 14:55:10 +0200

    tests/connection: reduce redundancy, match full lines

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,3 +1,4 @@
+import re
 import unittest
 from pathlib import Path
 
@@ -5,37 +6,15 @@ import lauterbach.trace32.pystart as pystart
 
 
 class TestCreateConnection(unittest.TestCase):
-    def testMCDConnectionWithStrParameter(self):
-        con = pystart.MCDConnection("some/string/path")
-        con._get_config_string(None)
-
-    def testMCDConnectionWithPathParameter(self):
-        con = pystart.MCDConnection(Path("some/other/path"))
-        con._get_config_string(None)
-
-    def testCADIConnectionWithStrParameter(self):
-        con = pystart.CADIConnection("some/string/path")
-        con._get_config_string(None)
-
-    def testCADIConnectionWithPathParameter(self):
-        con = pystart.CADIConnection(Path("some/other/path"))
-        con._get_config_string(None)
-
-    def testIRISConnectionWithStrParameter(self):
-        con = pystart.IRISConnection("some/string/path")
-        con._get_config_string(None)
-
-    def testIRISConnectionWithPathParameter(self):
-        con = pystart.IRISConnection(Path("some/other/path"))
-        con._get_config_string(None)
-
-    def testGDIConnectionWithStrParameter(self):
-        con = pystart.GDIConnection("some/string/path")
-        con._get_config_string(None)
-
-    def testGDIConnectionWithPathParameter(self):
-        con = pystart.GDIConnection(Path("some/other/path"))
-        con._get_config_string(None)
+    def testConnectionWithPathParameter(self):
+        connections = [pystart.MCDConnection, pystart.CADIConnection, pystart.IRISConnection, pystart.GDIConnection]
+        for con in connections:
+            with self.subTest(cls=con.__name__, type="str"):
+                c = con("some/string/path")
+                c._get_config_string(None)
+            with self.subTest(cls=con.__name__, type="Path"):
+                c = con(Path("some/path/object"))
+                c._get_config_string(None)
 
 
 class TestConnection(unittest.TestCase):
@@ -43,13 +22,13 @@ class TestConnection(unittest.TestCase):
         x = pystart.ViewerConnection()
 
         self.assertTrue(isinstance(x, pystart._connection._SingleConnection))
-        self.assertRegex(x._get_config_string(None), "PBI=VIEWER")
+        self.assertRegex(x._get_config_string(None), re.compile("^PBI=VIEWER$", flags=re.MULTILINE))
 
     def test_InteractiveConnection(self):
         x = pystart.InteractiveConnection()
 
         self.assertTrue(isinstance(x, pystart._connection._SingleConnection))
-        self.assertRegex(x._get_config_string(None), "PBI=INTERACTIVECONNECTION")
+        self.assertRegex(x._get_config_string(None), re.compile("^PBI=INTERACTIVECONNECTION$", flags=re.MULTILINE))
 
 
 if __name__ == "__main__":
